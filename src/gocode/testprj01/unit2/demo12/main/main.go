b5215e131492e87f4782dde4983a027c551c54c7
xiaozisong <1170957523@qq.com>
2024-03-22 00:11:37 +0800

    修改gopath及标识符的注意事项

---
+++ src/gocode/testprj01/unit2/demo12/main/main.go
@@ -0,0 +1,11 @@
+package main // 每个目录下都必须有一个main的主入口包，且包名最好与目录名保持一致
+
+import (
+	"fmt"
+	"gocode/testprj01/demo12/test"
+	// 如果想使用其他包的变量需要配置$GOPATH（工作目录）
+)
+
+func main() {
+	fmt.Println(test.StuName)
+}
---
+++ src/gocode/testprj01/unit2/demo12/test/test.go
@@ -0,0 +1,5 @@
+package test
+
+var stuNo int = 20034 // 如果标识符的第一个单词是小写，类似java中的private，只能在当前包中被访问
+
+var StuName byte = 'a' // 如果首字母大写，则可以在其他包中访问，类似java中的public
